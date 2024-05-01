04fb2eabbe9cca6be6a73397b54b95a4153ac361
nethmalgunawardhana <107822302+nethmalgunawardhana@users.noreply.github.com>
2024-05-01 15:59:46 +0530

    portfolio

---
+++ db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const dbURI = process.env.DB_URL;
+
+mongoose.connect(dbURI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+module.exports = mongoose;
